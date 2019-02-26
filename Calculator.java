e57590096164fa34d7b5daa1c721683be6c20d4e
RobertStrze <47864664+RobertStrze@users.noreply.github.com>
2019-02-26 02:39:22 +0100

    Add files via upload

--- Calculator.java
+++ Calculator.java
@@ -1,23 +1,46 @@
-import java.util.Scanner;
+//import java.util.Scanner;
 
 public class Calculator {
+    private static int[] fillAges() {
+        int[] ages = new int[12];
+        ages[0] = 61;
+        ages[1] = 31;
+        ages[2] = 44;
+        ages[3] = 26;
+        ages[4] = 65;
+        ages[5] = 11;
+        ages[6] = 5;
+        ages[7] = 47;
+        ages[8] = 82;
+        ages[9] = 12;
+        ages[10] = 16;
+        ages[11] = 95;
+
+
+        return ages;
+    }
+
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.print("First Number: ");
-        int x = input.nextInt();
-        System.out.print("Second Number: ");
-        int y = input.nextInt();
-        if(y == 0) {
-            System.out.println("You can't divide by 0");
-        }else{
-        System.out.println("x/y= " + div(x, y));
+        int[] ages = fillAges();
+        forLoop(ages);
+    }
+
+    private static void forLoop(int[] ages) {
+        int x = ages.length + 1;
+        for (int n = ages.length - 1; n >= 1; n--) {
+            x--;
+            if (n == ages.length - 1 && ages[n] > ages[n - 1] && ages[n] >= ages[ages.length - 1]) {
+                System.out.println("Peak = " + ages[n]);
+
+            } else if (n != ages.length - 1 && ages[n] > ages[n - 1] && ages[n] > ages[x]) {
+                System.out.println("Peak = " + ages[n]);
+
+            } else if (n == 1 && ages[n] < ages[0]) {
+                System.out.println("Peak = " + ages[n - 1]);
+            }
+
         }
-        System.out.println("x*y= " + mul(x, y));
+        System.out.println("Done -- forLoop");
     }
-    private static float div(float x, float y){
-        return x/y;
-    }
-    private static int mul(int x, int y){
-        return x*y;
-    }
+
 }
