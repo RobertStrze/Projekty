2d472e1d9f6c21c08c169920588ab83ea217faae
RobertStrze <47864664+RobertStrze@users.noreply.github.com>
2019-03-18 20:06:28 +0100

    Add files via upload

--- Lesson6.java
+++ Lesson6.java
@@ -1,34 +1,25 @@
 public class Lesson6 {
     public static void main(String[] args) {
         int[]myRef = {1,2,3};
-        System.out.print("Array(myRef): ");
-        for(int array: myRef){
-            System.out.print(array + " ");
-        }
-        System.out.println();
-        System.out.println("HashCodeMyRef: " + myRef.hashCode());
-        int []arrayRef = myRef;
-        arrayRef[0] = 2;
-        System.out.print("Array(arrayRef): ");
-        for(int array: arrayRef){
-            System.out.print(array + " ");
-        }
-        System.out.println();
-        int [] newArray = {4,5};
-        arrayRef = newArray;
-        System.out.println("HashCodeNewArray: " + newArray.hashCode());
         testRef(myRef);
-        System.out.print("Array(myRef): ");
+        System.out.println("HashCodeMyRef: " + myRef.hashCode());
+        System.out.print("MyRef: ");
         for(int array: myRef){
             System.out.print(array + " ");
         }
-        System.out.println();
-        System.out.println("HashCodeMyRef: " + myRef.hashCode());
-
 
     }
     static void testRef(int [] arrayRef){
         System.out.print("HashCodeArrayRef: " + arrayRef.hashCode());
+        System.out.println();
+        arrayRef[0] = 2;
+        arrayRef = new int[]{4,5};
+        System.out.println("HashCodeArrayRef: " + arrayRef.hashCode());
+        System.out.print("ArrayRef: ");
+        for(int array: arrayRef){
+            System.out.print(array + " ");
+        }
+
         System.out.println();
     }
 }
